961f413b6e7f2389683dc1a4d7669a147caa241c
david-rose <dar@acm.org>
2015-08-01 09:33:40 -0400

    data iteration

--- web/data/electricity.js
+++ web/data/electricity.js
@@ -82,22 +82,6 @@ function getflowgroups(flow)
     return flowgroups;
 }
 
-function getflowtypes_unused(flow)
-{
-    var flowgroups = getflowgroups(flow);
-    var flowtypes = {};
-    for (var i = 0; i < flowgroups.length; i++)
-    {
-        flowgroup = flowgroups[i];
-        for (j = 0; j < flowgroup.length; j++)
-        {
-            flowtypes[flowgroup[j]] = 1;
-        }
-    }
-    console.log('getflowtypes_unused: ' + Object.keys(flowtypes).sort());
-    return Object.keys(flowtypes).sort();
-}
-
 function getflowtypes(flow)
 {
     var flowlist = [];
@@ -158,19 +142,25 @@ function getwheeldata(names, years, valuetype, adjoining)
     for (var i = 0; i < yearlist.length; i++)
     {
         var year = yearlist[i];
-        if (isnumeric(year))
+        var r = new Object();
+        r['name'] = region;
+        r['year'] = year;
+        r['selected'] = (year == selectedyear) ? 'Y' : 'N';
+        if (year >= minyear && year <= maxyear)
         {
             var facts = data[region][year];
-            var r = new Object();
-            r['name'] = region;
-            r['year'] = year;
-            r['selected'] = (year == selectedyear) ? 'Y' : 'N';
             r['population'] = facts.population;
             var units = getunits(valuetype, r.population);
-            r['electricity'] = Math.round(units.mult * facts.electricity);
+            r['electricity'] = (units.mult * facts.electricity);
             r['units'] = units.units;
-            wheeldata.push(r);
         }
+        else
+        {
+            r['population'] = "";
+            r['electricity'] = "";
+            r['units'] = "";
+        }
+        wheeldata.push(r);
     }
     return wheeldata;
 }
@@ -183,12 +173,15 @@ function getyearlist(selectedyear, adjoining)
     // if adjoining < 0, return all years
     var selectedyear = parseInt(selectedyear);
     var yearlist = [];
-    var start = minyear;
-    var end = maxyear;
+    var padding = 3
+    var start = minyear - padding;
+    var end = maxyear + padding;
     if (adjoining >= 0)
     {
-        start = Math.max(selectedyear - adjoining, minyear);
-        end =   Math.min(selectedyear + adjoining, maxyear);
+        // start = Math.max(selectedyear - adjoining, minyear);
+        // end =   Math.min(selectedyear + adjoining, maxyear);
+        start = selectedyear - adjoining;
+        end =   selectedyear + adjoining;
     }
     for (var i = start; i <= end; i++)
     {
@@ -209,13 +202,13 @@ function setpercent(level, target)
         var p = 0;
         if (total > 0)
         {
-            p = Math.round(level[i]['value'] / total * target);
+            p = (level[i]['value'] / total * target);
         }
         level[i]['percent'] = p;
-        var ch = level[i]['children'];
-        if (typeof ch == 'object' && level[i]['children'].length > 0)
+        var children = level[i]['children'];
+        if (typeof children == 'object' && children.length > 0)
         {
-            setpercent(level[i]['children'], p);
+            setpercent(children, p);
         }
     }
 }
@@ -241,8 +234,8 @@ function query(names, years, valuetype, flows, hierarchy)
             r['issubregion'] = nyears.issubregion;
             r['region'] = nyears.region;
             r['subregion'] = nyears.subregion;
-            r['renewable'] = Math.round(units.mult * facts.renewable);
-            r['nonrenewable'] = Math.round(units.mult * facts.nonrenewable);
+            r['renewable'] = (units.mult * facts.renewable);
+            r['nonrenewable'] = (units.mult * facts.nonrenewable);
             r['units'] = units.units;
             curdata.push(getAttributes(r, facts, units, flows, hierarchy));
             if (nyears.isregion == 'Y')
@@ -282,55 +275,44 @@ function getAttributes(r, facts, units, flows, hierarchy) {
     for (var flow in flowtypes)
     {
         // if value == 0 should we not include it in list?
-        details.push({
-            "name": key,
-            "value": Math.round(units.mult * facts[flow]),
-            "units": units.units,
-            "desc": getdescription(flow)
-        });
+        if (facts[flow] > 0)
+        {
+            details.push({
+                "name": key,
+                "value": (units.mult * facts[flow]),
+                "units": units.units,
+                "desc": getdescription(flow)
+            });
+        }
     }
     r["details"] = details;
     return r;
 }
 
-function init()
+
+var SankeyObj = function(otype)
 {
-    data = loaddata();
-    updatedata();
+    this.otype = otype; // 'energy' or 'region'
+    this.georegion = '';
+    this.flow = '';
+    this.name = '';
+    this.description = '';
+    this.year = '';
+    this.value = 0;
+    this.units = '';
+    this.percent = 0;
+    this.ctype = ''; // type of child objects
+    this.children = [];
+}
+var SankeyObjMin = function(o)
+{
+    this.name = o.name
+    this.description = o.description;
+    this.value = o.value;
+    this.percent = o.percent;
+    this.children = o.children;
 }
 
-
-function updatedata()
-{
-    var regions = getmultiples(document.getElementById("region"));
-    var years = getmultiples(document.getElementById("year"));
-    var valuetype = document.getElementById("units").value;
-    var flows = getmultiples(document.getElementById("flow"));
-    var hierarchy = document.getElementById("hierarchy").value;
-    var adjoiningyears = parseInt(document.getElementById("adjoining").value);
-
-    var wheeldata = getwheeldata(regions, years, valuetype, adjoiningyears);
-    var maxlevels = 2;
-    var sankeydata = getsankeydata(regions[0], years[0], valuetype, flows[0], hierarchy, maxlevels);
-    drawdata([wheeldata, sankeydata], ['Wheel Data', 'Sankey Data']);
-}
-
-function drawdata(dataarray, labels)
-{
-    for (var i = 0; i < dataarray.length; i++)
-    {
-        var thisdata = dataarray[i];
-        var s = "";
-        thisdata.forEach(function(item, index, array)
-                         {
-                             s = s + JSON.stringify(item, null, '  ') + "\n";
-                         });
-        s = "<hr/><br/><strong>" + labels[i] + "</strong><br/><hr/>" + "<pre>" + s;
-        s = s + "</pre>" + "<hr/>";
-        var c = "code" + (i + 1);
-        document.getElementById(c).innerHTML = s;
-    }
-}
 
 function getsankeydata(name, selectedyear, valuetype, flow, hierarchy, maxlevels)
 {
@@ -342,20 +324,26 @@ function getsankeydata(name, selectedyear, valuetype, flow, hierarchy, maxlevels
 
     if (hierarchy == 'energy')
     {
-        var year = data[name][selectedyear];
-        var units = getunits(valuetype, year.population);
+        var yeardata = data[name][selectedyear];
+        var units = getunits(valuetype, yeardata.population);
 
         for (var i = 0; i < flowgroups.length; i++)
         {
             var cflow = flowgroups[i];
-            var r = new Object();
-            r['name'] = name;
-            r['year'] = selectedyear;
-            r['value'] = Math.round(units.mult * year[cflow]);
-            r['units'] = units.units;
-            r['description'] = getdescription(cflow); // NOTE: need to create descriptions for higher-level energy flows
-            r['children'] = getsankeychildrenregions(name, selectedyear, cflow, units);
-            sankeydata.push(r);
+            if (yeardata[cflow] > 0)  // exclude records with zero value
+            {
+                var r = new SankeyObj('energy');
+                r.georegion = name;
+                r.flow = cflow;
+                r.name = name;
+                r.description = getdescription(cflow);
+                r.year = selectedyear;
+                r.value = (units.mult * yeardata[cflow]);
+                r.units = units.units;
+                r.children = getsankeychildrenregions(r, cflow, units);
+                r.ctype = 'region';
+                sankeydata.push(r);
+            }
         }
     }
     else if (hierarchy == 'region')
@@ -371,65 +359,176 @@ function getsankeydata(name, selectedyear, valuetype, flow, hierarchy, maxlevels
         }
         else
         {
-            // this is a country, no further geographic details available
+            // this is a country, list is empty
             regionlist = [];
+            var yeardata = data[name][selectedyear];
+            var units = getunits(valuetype, yeardata.population);
+            var r = new SankeyObj('region');
+            r.georegtion = name;
+            r.flow = flow;
+            r.name = name;
+            r.year = selectedyear;
+            sankeydata = getsankeychildrenenergy(r, units);
         }
         for (var i = 0; i < regionlist.length; i++)
         {
             var cflow = 'electricity';
             var cregion = regionlist[i];
-            var year = data[cregion][selectedyear];
-            var units = getunits(valuetype, year.population);
-            var r = new Object();
-            r['name'] = cregion;
-            r['year'] = selectedyear;
-            r['value'] = Math.round(units.mult * year[cflow]);
-            r['units'] = units.units;
-            r['description'] = getdescription(cflow); // NOTE: need to create descriptions for higher-level energy flows
-            r['children'] = getsankeychildrenenergy(cregion, selectedyear, flow, units);
-
-            sankeydata.push(r);
+            var yeardata = data[cregion][selectedyear];
+            if (yeardata[cflow] > 0) // exclude records with zero value
+            {
+                var units = getunits(valuetype, yeardata.population);
+                var description = getdescription(cflow);
+                var r = new SankeyObj('region');
+                r.georegion = name;
+                r.flow = flow;
+                r.name = cregion;
+                r.year = selectedyear;
+                r.value = (units.mult * yeardata[cflow]);
+                r.units = units.units;
+                r.description = description;
+                r.children = getsankeychildrenenergy(r, units);
+                r.ctype = 'energy';
+                sankeydata.push(r);
+            }
         }
-        
     }
     setpercent(sankeydata, 100);
-    return sankeydata;
+    var tmp = new SankeyObj(hierarchy);
+    tmp.georegion = name;
+    tmp.flow = flow;
+    tmp.year = selectedyear;
+    tmp.units = getunits(valuetype, 1).units;
+    // collapse small percentage records into an aggregate record
+    sankeydata = aggregate(sankeydata, tmp);
+    //    return sankeydata;
+    return simplify(sankeydata);
 }
 
-function getsankeychildrenenergy(name, selectedyear, flow, units)
+function simplify(o)
+{
+    for (var i = 0; i < o.length; i++)
+    {
+        var v = o[i];
+        if (typeof v.children == 'object' && v.children.length > 0)        {
+            v.children = simplify(v.children);
+        }
+    }
+    var newlist = [];
+    for (var i = 0; i < o.length; i++)
+    {
+        newlist.push(new SankeyObjMin(o[i]));
+    }
+    return newlist;
+}
+var lowthreshold = 2; // minimum percent value, anything less gets aggregated
+var maxsankey = 5; // maximum number of individual records to display, aggregate the rest
+function aggregate(o, proto)
+{
+    for (var i = 0; i < o.length; i++)
+    {
+        var v = o[i];
+        if (typeof v.children == 'object' && v.children.length > 0)
+        {
+            v.children = aggregate(v.children, v);
+        }
+    }
+    // sort in order of percent
+    o.sort(sortbypercent);
+    var newlist = [];
+    var r = new SankeyObj(proto.otype);
+    r.georegion = proto.georegion;
+    r.flow = proto.flow;
+    r.name = 'Other';
+    r.year = proto.year;
+    r.units = proto.units;
+    for (var i = 0; i < o.length; i++)
+    {
+        var v = o[i];
+        var p = v.percent;
+        if (p < lowthreshold || i >= maxsankey)
+        {
+            r.value += v.value;
+            r.description += (r.description == '') ? v.name : ', ' + v.name;
+            r.percent += v.percent;
+        }
+        else
+        {
+            newlist.push(v);
+        }
+        v.percent = v.percent.toFixed(2);
+    }
+    if (r.value > 0)
+    {
+        r.percent = r.percent.toFixed(2);
+        newlist.push(r);
+    }
+    return newlist;
+}
+
+function sortbypercent(x, y)
+{
+    // sort by 'percent' field in decreasing order
+    return (sortby(x, y, 'percent') * -1);
+}
+
+function sortby(x, y, field)
+{
+    var xp = x[field];
+    var yp = y[field];
+    r = 0; // default is equals
+    if (xp < yp)
+    {
+        r = -1;
+    }
+    else if (xp > yp)
+    {
+        r = 1;
+    }
+    return r;
+}
+
+function getsankeychildrenenergy(parent, units)
 {
     var children = [];
-    var region = data[name];
-    var flowgroups = getflowgroups(flow);
-    var year = data[name][selectedyear];
+    var region = data[parent.name];
+    var flowgroups = getflowgroups(parent.flow);
+    var yeardata = data[parent.name][parent.year];
     for (var i = 0; i < flowgroups.length; i++)
     {
-        var cflow = flowgroups[i];
-        var r = new Object();
-        r['name'] = name;
-        r['year'] = selectedyear;
-        r['value'] = Math.round(units.mult * year[cflow]);
-        r['units'] = units.units;
-        r['description'] = getdescription(cflow); // NOTE: need to create descriptions for higher-level energy flows
-        r['children'] = getsankeyenergydetails(region, year, cflow, units);
-        children.push(r);
+        var flow = flowgroups[i];
+        if (yeardata[flow] > 0) // exclude records with zero value
+        {
+            var r = new SankeyObj('energy');
+            r.georegion = parent.name;
+            r.flow = flow;
+            r.name = flow;
+            r.year = parent.year;
+            r.value = (units.mult * yeardata[flow]);
+            r.units = units.units;
+            r.description = getdescription(flow);
+            r.children = getsankeyenergydetails(r, region, yeardata, flow, units);
+            //r.children = getsankeychildrenregions(r, flow, units);
+            r.ctype = 'energy';
+            children.push(r);
+        }
     }
     return children;
 }
 
-function getsankeychildrenregions(name, selectedyear, flow, units)
+function getsankeychildrenregions(parent, flow, units)
 {
     
     var children = [];
     var regionlist = [];
-    var region = data[name];
+    var region = data[parent.georegion];
     if (region.isregion == 'Y')
     {
-        regionlist = Object.keys(regions[name]).sort();
+        regionlist = Object.keys(regions[parent.name]).sort();
     }
     else if (region.issubregion == 'Y')
     {
-        regionlist = regions[region['region']][name]; // this is confusing
+        regionlist = regions[region['region']][parent.name]; // this is confusing
     }
     else
     {
@@ -438,36 +537,46 @@ function getsankeychildrenregions(name, selectedyear, flow, units)
     }
     for (i = 0; i < regionlist.length; i++)
     {
-        var cname = regionlist[i];
-        var cregion = data[cname];
-        var cyear = data[cname][selectedyear];
-        var value = cyear[flow];
-        r = new Object();
-        r['name'] = cname;
-        r['value'] = Math.round(units.mult * value);
-        r['units'] = units.units;
-        r['description'] = getdescription(flow);
-        r['children'] = getsankeyenergydetails(cregion, cyear, flow, units);
-        children.push(r);
+        var name = regionlist[i];
+        var cregion = data[name];
+        var yeardata = data[name][parent.year];
+        if (yeardata[flow] > 0) // exclude records with zero value
+        {
+            var r = new SankeyObj('region');
+            r.georegion = name;
+            r.flow = parent.flow
+            r.name = name;
+            r.year = parent.year;
+            r.value = (units.mult * yeardata[flow]);
+            r.units = units.units;
+            r.description = getdescription(flow);
+            r.children = getsankeyenergydetails(r, cregion, yeardata, flow, units);
+            r.ctype = 'energy';
+            children.push(r);
+        }
     }
     return children;
 }
 
-function getsankeyenergydetails(region, year, flow, units)
+function getsankeyenergydetails(parent, region, yeardata, flow, units)
 {
     var details = [];
     var flowtypes = getflowtypes(flow);
     for (var i = 0; i < flowtypes.length; i++)
     {
-        var r = new Object();
         var flow = flowtypes[i];
-        var value = year[flow];
-        r['name'] = flow;
-        r['value'] = Math.round(units.mult * value);
-        r['units'] = units.units;
-        r['description'] = getdescription(flow);
-        r['children'] = [];
-        details.push(r);
+        if (yeardata[flow] > 0) // exclude records with zero value
+        {
+            var r = new SankeyObj('energy');
+            r.georegion = parent.georegion;
+            r.flow = flow;
+            r.name = flow;
+            r.description = getdescription(flow);
+            r.year = parent.year;
+            r.value = (units.mult * yeardata[flow]);
+            r.units = units.units;
+            details.push(r);
+        }
     }
     return details;
 }
